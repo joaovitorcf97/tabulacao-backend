39e4c787b6a3a0a53a453d685f01fc8b509bd026
João Vitor <joaovitorcfdev@gmail.com>
2022-10-20 07:58:50 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
+import cors from 'cors';
 import express, { NextFunction, Request, Response } from "express";
 import 'express-async-errors';
 import { routes } from "./routes";
 
+
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(routes);
